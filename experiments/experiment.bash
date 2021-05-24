5dc8436124b1904268743b342393907c27db22fb
Alexey Mamaev <a.mamaev@corp.mail.ru>
2021-05-24 22:53:06 +0300

    PDG: [*] refactoring

--- experiments/experiment.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -x "$1" ]] || [[ -e "$2" ]]; then
-    echo "Usage: $0 [[PROGRAM]] [[JOB]]"
-    exit 1
-fi
-
-parallel --colsep '	' -a "$2" "$(dirname "$0")./run.bash" {1} {2} {3} {4} {5} {6} {7} {8} "$1"
